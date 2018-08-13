c78c54cc50cfe065dd0529adaafd0cf2bc8fcf81
Cédric JUNG <cedced19@gmail.com>
2018-08-13 18:47:09 +0200

    Display total and add functions to add points

--- www/js/app.js
+++ www/js/app.js
@@ -13,35 +13,88 @@ phonon.navigator().on({
     content: 'home.html',
     preventClose: false,
     readyDelay: 0
-}, function(activity) {
-    var nous = [78, 36, 124, 62];
-    var vous = [84, 126, 38, 100];
+}, function (activity) {
 
-    activity.onCreate(function() {
+    activity.onCreate(function () {
+
         var scoreDiv = document.getElementById('score');
+        var scoreInput = document.getElementById('score-input');
+        var nous = JSON.parse(localStorage.getItem('nous')) || [];
+        var vous = JSON.parse(localStorage.getItem('vous')) || [];
 
-        for (var i = 0; i< nous.length; i++) {
+
+        function getTotal (list, id) {
+            var count = 0;
+            for (var k = 0; k < id; k++) {
+                count += list[k];
+            }
+            return count;
+        }
+
+        function showScore(id, n, v) {
             var mainDiv = document.createElement('div');
             mainDiv.classList.add('row', 'text-center', 'table');
 
             var idDiv = document.createElement('div');
-            idDiv.innerHTML = i+1;
+            idDiv.innerHTML = id;
             idDiv.classList.add('tablet-2', 'large-2', 'phone-2', 'column', 'table-item');
             mainDiv.appendChild(idDiv);
 
             var nousDiv = document.createElement('div');
-            nousDiv.innerHTML = nous[i];
+            nousDiv.innerHTML = getTotal(nous, id);
             nousDiv.classList.add('tablet-5', 'large-5', 'phone-5', 'column', 'table-item');
             mainDiv.appendChild(nousDiv);
 
             var vousDiv = document.createElement('div');
-            vousDiv.innerHTML = vous[i];
+            vousDiv.innerHTML = getTotal(vous, id);;
             vousDiv.classList.add('tablet-5', 'large-5', 'phone-5', 'column', 'table-item');
             mainDiv.appendChild(vousDiv);
 
             scoreDiv.appendChild(mainDiv);
         }
 
+
+        for (var i = 0; i < nous.length; i++) {
+            showScore(i + 1, nous[i], vous[i])
+        }
+
+        document.getElementById('add-nous').on('click', function() {
+            var valN = Number(scoreInput.value);
+            var valV = 162 - Number(scoreInput.value);
+            if (belote.checked) {
+                valN+= 20;
+            }
+            if (valN>250) valV = 0;
+            nous.push(valN);
+            vous.push(valV);
+            localStorage.setItem('nous', JSON.stringify(nous));
+            localStorage.setItem('vous', JSON.stringify(vous));
+            showScore(nous.length, valN, valV);
+        });
+        
+        document.getElementById('add-vous').on('click', function() {
+            var valV = Number(scoreInput.value);
+            var valN = 162 - Number(scoreInput.value);
+            if (belote.checked) {
+                valV+= 20;
+            }
+            if (valV>250) valN = 0;
+            nous.push(valN);
+            vous.push(valV);
+            localStorage.setItem('nous', JSON.stringify(nous));
+            localStorage.setItem('vous', JSON.stringify(vous));
+            showScore(nous.length, valN, valV);
+        });
+
+        document.getElementById('reset').on('click', function() {
+            localStorage.setItem('nous', '[]');
+            nous = [];
+            localStorage.setItem('vous', '[]');
+            vous = [];
+            while (scoreDiv.firstChild) {
+                scoreDiv.removeChild(scoreDiv.firstChild);
+            }
+        });
     });
 });
 
